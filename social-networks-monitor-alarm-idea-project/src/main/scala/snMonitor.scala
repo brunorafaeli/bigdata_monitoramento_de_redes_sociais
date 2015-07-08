f9723541d822b4dd34a9259151e4775cc8d78f0c
Pedro Miranda <miranda.pp@gmail.com>
2015-07-07 22:50:35 -0300

    Disparador de alarmes cloeses #6

--- social-networks-monitor-alarm-idea-project/src/main/scala/snMonitor.scala
+++ social-networks-monitor-alarm-idea-project/src/main/scala/snMonitor.scala
@@ -3,23 +3,22 @@
  */
 /* snMonitor.scala */
 
-import org.apache.spark.SparkContext._
-import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.api.java.function.Function2
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 import scala.collection.JavaConversions._
 
 object snMonitor {
   def main(args: Array[String]) {
-    val HISTORICINTERVAL = 10
-    val RECENTICINTERVAL= 5
+
+    //Get parameters
     val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
-    val dictfilenm = args(4)
-    val outdir = args(5)
-    //val filters = args.takeRight(args.length - 4)
+    val dictfilenm = args(4) //filepath of a dictionary with company's categories terms
+    val outdir = args(5) //output directory
+    val historicinterval = args(6).toInt //in minutes
+    val recenticinterval= args(7).toInt //in minutes
+    val stubport = args(8).toInt //set 0 to disable stub
+    val limit_stdev_cof = args(9).toDouble //in minutes
 
     // Set the system properties so that Twitter4j library used by twitter stream
     // can use them to generat OAuth credentials
@@ -27,31 +26,38 @@ object snMonitor {
     System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
     System.setProperty("twitter4j.oauth.accessToken", accessToken)
     System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+    //Read dictionary with company's categories terms
     val dictCompany = new Company(dictfilenm)
     val filters = dictCompany.getTermsArray
+
+    //Conf scc
     val conf = new SparkConf()
       .setMaster("local[3]")
       .setAppName("snMonitor Application")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(15))
     
-    //Stub Configurations
-    val stubstream = ssc.socketTextStream("localhost", 9999)
-    val stubcollector = stubstream.map(text => (dictCompany.getName,text))
+
     //Twitter Configurations
     val twstream = TwitterUtils.createStream(ssc, None, filters)
     val twcollector = twstream.map(status => (dictCompany.getName,status.getText))
 
-
     //unifiedStream aggregates all input streams
-   // val unifiedStream = tw_txt.union()
-    val unifiedStream = twcollector.union(stubcollector)
-    val words = unifiedStream.flatMapValues(_.toLowerCase.split("[^a-zA-Z0-9@#-]"))
-    words.print()
-    //gets categories
-    //val wordCategories = dictCompany.wordCategory()
+    val unifiedStream =  if(stubport == 0){
+      twcollector
+    }else {
+      //Add a stub stream listening to socket on stubport
+      val stubstream = ssc.socketTextStream("localhost", stubport)
+      val stubcollector = stubstream.map(text => (dictCompany.getName, text))
+      twcollector.union(stubcollector)
+    }
     unifiedStream.print()
-    val pairs = words.map({case (company,word) => ((company,word), 1.0)})
+
+    //Split text in words
+    val words = unifiedStream.flatMapValues(_.toLowerCase.split("[^a-zA-Z0-9@#-]"))
+
+    //Emit categories related to a word
     val categories = words.flatMap({case (company, word) => (dictCompany.wordCategory(word)).map(cat => ((company, cat),1.0))})
 
     //Gets tuples with a 0.0 for each category
@@ -69,32 +75,30 @@ object snMonitor {
     //Creates a union between categories and zerosRDD
     val categoriesWithZeros = categories.transform(rdd => rdd.union(zerosRDD))
 
+    //Sum categories count
     val categoryCounts = categoriesWithZeros.reduceByKey(_+_)
-    
-    val countWriter = new CountWriter(outdir)
-    categoryCounts.foreachRDD((rdd,time) =>  countWriter.appendCategoryCount(rdd.collect(),time))
-    
-    val categoryCountsHistoricWindow = categoryCounts.window(Minutes(HISTORICINTERVAL), Seconds(15)).groupByKey
-    val categoryCountsRecentWindow = categoryCounts.window(Minutes(RECENTICINTERVAL), Seconds(15)).groupByKey
+
+    //Write categories count log to a file
+    val monitorWriter = new MonitorWriter(outdir)
+    categoryCounts.foreachRDD((rdd,time) =>  monitorWriter.appendCategoryCount(rdd.collect(),time))
+
+    //Compute stats for a short time window and long time window
+    val categoryCountsHistoricWindow = categoryCounts.window(Minutes(historicinterval), Seconds(15)).groupByKey
+    val categoryCountsRecentWindow = categoryCounts.window(Minutes(recenticinterval), Seconds(15)).groupByKey
     val historicStat = categoryCountsHistoricWindow.mapValues( value => org.apache.spark.util.StatCounter(value))
     val recentStat = categoryCountsRecentWindow.mapValues( value => org.apache.spark.util.StatCounter(value))
     val stats = historicStat.join(recentStat)
-    //stats.print()
+    stats.print()
 
-    //val wordCounts = pairs.reduceByKey(_+_)
-    //val wordCountsHistoricWindow = wordCounts.window(Minutes(HISTORICINTERVAL), Seconds(15)).groupByKey
-    //val wordCountsRecentWindow = wordCounts.window(Minutes(RECENTICINTERVAL), Seconds(15)).groupByKey
-    //val historicStat = wordCountsHistoricWindow.mapValues( value => org.apache.spark.util.StatCounter(value))
-    //val recentStat = wordCountsRecentWindow.mapValues( value => org.apache.spark.util.StatCounter(value))
-    //val stats = historicStat.join(recentStat)
+    //Get only critical categories stats
+    val critical_stats = stats.filter(({case ((company,category),(historicStat,recentStat)) =>
+                                          recentStat.mean > (historicStat.mean + (limit_stdev_cof * historicStat.stdev))
+                                      }))
 
-    // Print the first ten elements of each RDD generated in this DStream to the console
-    //wordCounts.print()
-    //unifiedStream.print()
-    //historicStat.print()
-    //recentStat.print()
-    //val s = stats.foreachRDD(value =>print(value))
-    stats.foreachRDD(rdd => rdd.foreach(rdd => println(rdd.toString())))
+    //Activate alarm for critical categories stats
+    critical_stats.foreachRDD((rdd,time) =>  monitorWriter.activateAlarm(rdd.collect(),time))
+
+
     ssc.start()
     ssc.awaitTermination()
   }
